d81662dd8c26e172d2faf496468f5eb398dd1d51
Galaxeo <hwjustincheok@gmail.com>
2024-06-14 18:33:30 -0400

    feat: add calendar + input ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { Input } from "@/components/ui/input";
+import { Calendar } from "./components/ui/calendar";
+import { useState } from "react";
 
 function App() {
+  const [date, setDate] = useState<Date[] | undefined>([]);
   return (
     <>
       <h1>Clare</h1>
       <p>不能再犹豫了...</p>
+      <Input />
+      <Calendar mode="multiple" selected={date} onSelect={setDate} />
     </>
   );
 }
