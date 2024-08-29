4faa754f07f00566e745431bdf65a7c87005359b
Galaxeo <hwjustincheok@gmail.com>
2024-08-28 18:33:38 -0700

    feat: add custom fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import "./App.css";
 import { Input } from "@/components/ui/input";
-import { Calendar } from "./components/ui/calendar";
+import { Calendar } from "@/components/ui/calendar";
 import { useState } from "react";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   const [date, setDate] = useState<Date[] | undefined>([]);
   return (
     <>
-      <h1>Clare</h1>
-      <p>不能再犹豫了...</p>
-      <Input />
-      <Calendar mode="multiple" selected={date} onSelect={setDate} />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <h1>Clare</h1>
+        <p>不能再犹豫了...</p>
+        {/* <Input />
+        <Calendar mode="multiple" selected={date} onSelect={setDate} /> */}
+      </ThemeProvider>
     </>
   );
 }
