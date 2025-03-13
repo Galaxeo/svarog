2b257c25986e70e2f4464fd699302621206c6b8b
Galaxeo <hwjustincheok@gmail.com>
2025-03-13 14:53:46 -0700

    progress indicator

--- components/FreeTimer.tsx
+++ components/FreeTimer.tsx
@@ -2,6 +2,7 @@ import { Text, View, StyleSheet, Touchable } from "react-native";
 import { useState, useEffect, useRef } from "react";
 
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import CircularProgress, { CircularProgressBase } from 'react-native-circular-progress-indicator';
 import { s, colors } from "@/app/styles";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Platform, TouchableOpacity } from "react-native";
@@ -10,7 +11,6 @@ import Recall from "@/components/Recall/Recall";
 import Settings from "@/components/PomodoroSettings";
 import Statistics from "@/components/Statistics";
 import DynamicAssist from "@/components/DynamicAssist";
-import { supabase } from "@/supabase";
 import { useAudioPlayer } from 'expo-audio'
 
 const notificationSound = require("@/assets/notification.mp3");
@@ -64,11 +64,17 @@ export default function FreeTimer() {
   // Other functions
   function formatTime() {
     // format the time to be displayed in minutes and seconds
+
   }
+  const baseProps = { activeStrokeWidth: 10, inActiveStrokeWidth: 10, activeStrokeColor: colors.text, inActiveStrokeColor: colors.darkGray2, showProgressValue: false, maxValue: 60 };
   // TODO: Setup break to be toggled on/off while running regular time, and format time to be displayed well
   return (
     <View>
-      <Text style={s.text}>{time}</Text>
+      <CircularProgressBase {...baseProps} value={Math.floor(time / 60)} radius={100}>
+        <CircularProgressBase {...baseProps} value={time} radius={90}>
+        </CircularProgressBase>
+      </CircularProgressBase>
+      <Text style={s.text}>{Math.floor(time / 60)}:{time % 60}</Text>
       {isActive ? (
         <TouchableOpacity onPress={pauseStopwatch}>
           <Text style={s.text}>Stop</Text>
